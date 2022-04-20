23d5f6c38403c27904708c3fa09ee33c1ee49aa7
Sevescen <92009435+Sevescen@users.noreply.github.com>
2022-04-19 23:48:30 -0400

    made readfile to read csv

--- readDataFile.h
+++ readDataFile.h
@@ -1,3 +1,35 @@
-void readMubiFile () {
+#include<string>
+#include<vector>
+#include<fstream>
+#include<iostream>
+using namespace std;
+
+vector<string> readMubiFile (string fileName) {
+     string movieID;
+     string title;
+     string releaseDate;
+     string popularity;
+     string directorID;
+     string directorName;
         
+     vector<string> movieData;
+     
+     ifStream inFile;
+     inFile.open(fileName);
+     if(!inFile.is_open()){
+             cout<<"Warning: File failed to open"<<endl;
+     }
+     
+     while(!inFile.eof()){
+             movieData.push_back(getline(inFile, movieID,','));
+             getline(inFile, title,',');
+             getline(inFile, releaseDate,',');
+             getline(inFile, popularity,',');
+             getline(inFile, directorID,',');
+             getline(inFile, directorName,',');
+     }
+     
+//      for(int i = 0; i < 6; i++){
+//              movieData.push_back()
+//      }
 }
