53780e24c0eb79d7d350df639deda709ad35a00a
ruben-hernandez-jr <ruben.hernandez@ufl.edu>
2022-04-23 20:00:35 -0400

    Adjusted whitespace and finalized menu options

--- foscarsMain.cpp
+++ foscarsMain.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include <string>
+#include <iostream>
 #include "readDataFileTest.h"
-/*#include "heapImplementationTest.h"
-#include "mapImplementation.h" */
+#include "heapImplementationTest.h"
+#include "mapImplementation.h" 
 
 
 
 using namespace std;
 void printMenu() {
+    cout << "--------------------" << endl;
+    cout << "0. Exit\n";
     cout << "1. Print 10 Popular Movies\n";
     cout << "2. Print 10 Non-Popular Movies\n";
-    cout << "3. Search by title\n";
-    cout << "4. Search by year \n";
-    cout << "5. Search by director\n";
+    cout << "3. Search by Title\n";
+    cout << "4. Search by Year \n";
+    cout << "5. Search by Range of Years\n";
+    cout << "6. Search by Director\n";
+    cout << "\nSelect a Menu Option: ";
 }
 
 int main() {
@@ -20,9 +26,10 @@ int main() {
     vector<Movie> movies;
 
     movies = readFile("movie_data.csv");
+
     bool exit = false;
     int n = 0;
-    string t, y;
+    string t, y, y1, p;
     movieMap map = movieMap();
     map.addCollection(movies);
 
@@ -32,6 +39,12 @@ int main() {
         min.insertMovie(movies[i]);
         max.insertMovie(movies[i]);
     }
+    minHeap unpopularMovies = minHeap();
+    maxHeap popularMovies = maxHeap();
+    minHeap minYear = minHeap();
+    maxHeap maxYear = maxHeap();
+    vector<Movie> year;
+    vector<Movie> dirMovies;
     while (exit != true) {
         printMenu();
         cin >> n;
@@ -41,38 +54,128 @@ int main() {
             exit = true;
             break;
         case 1:
-            for (int i = 0; i < 10; i++) {
-                Movie m = max.extractMax();
+            cout << endl;
+            popularMovies = max;
+            for (unsigned int i = 0; i < 10; i++) {
+                Movie m = popularMovies.extractMax();
                 cout << m.getTitle() << "(" << m.getYear() << ")" << endl;
-                cout << "Popularity Score: " << m.getPopularity();
+                cout << "Popularity Score: " << m.getPopularity() << endl;
+                cout << endl;
             }
 
             break;
         case 2:
+            cout << endl;
+            unpopularMovies = min;
             for (int i = 0; i < 10; i++) {
-                Movie m = min.extractMin();
+                Movie m = unpopularMovies.extractMin();
                 cout << m.getTitle() << "(" << m.getYear() << ")" << endl;
-                cout << "Popularity Score: " << m.getPopularity();
+                cout << "Popularity Score: " << m.getPopularity() << endl;
+                cout << endl;
             }
             break;
         case 3:
-            cout << "Insert Title" << endl;
-            cin >> t;
+            cout << endl;
+            cout << "Insert Title:\n" << endl;
+            getline(cin, t);
+            getline(cin, t);
             map.getInfo(t);
             break;
         case 4:
-            cout << "Insert Year" << endl;
+            cout << endl;
+            cout << "Insert Year: ";
             cin >> y;
-            map.printMovie(map.getMovieYear(map.mMap, y));
+            year = map.getMovieYear(map.mMap, y);
+            minYear.resetMinHeap();
+            maxYear.resetMaxHeap();
+            for (int i = 0; i < year.size(); i++) {
+                minYear.insertMovie(year[i]);
+                maxYear.insertMovie(year[i]);
+            }
+            cout << endl;
+            cout << "1. More Popular" << endl;
+            cout << "2. Less Popular" << endl;
+            cout << "Choose Popularity Level: ";
+            cin >> p;
+            cout << endl;
+            if (p == "1") {
+                for (unsigned int i = 0; i < 10; i++) {
+                    Movie m = maxYear.extractMax();
+                    cout << m.getTitle() << " (" << m.getYear() << ")";
+                    cout << " - Dir. by: " << m.getDirectorName() << endl;
+                    cout << "Popularity Score: " << m.getPopularity() << endl;
+                    cout << endl;
+                }
+            }
+            else if (p == "2") {
+                for (int i = 0; i < 10; i++) {
+                    Movie m = minYear.extractMin();
+                    cout << m.getTitle() << " (" << m.getYear() << ")";
+                    cout << " - Dir. by: " << m.getDirectorName() << endl;
+                    cout << "Popularity Score: " << m.getPopularity() << endl;
+                    cout << endl;
+                }
+            }
             break;
         case 5:
-            cout << "Insert Director" << endl;
+            cout << endl;
+            cout << "Insert First Year: ";
+            cin >> y;
+            cout << "Insert Last Year: ";
+            cin >> y1;
+            minYear.resetMinHeap();
+            maxYear.resetMaxHeap();
+            for (int i = stoi(y); i <= stoi(y1); i++) {
+                year = map.getMovieYear(map.mMap, to_string(i));
+                for (int j = 0; j < year.size(); j++) {
+                    minYear.insertMovie(year[j]);
+                    maxYear.insertMovie(year[j]);
+                }
+            }
+            cout << "\n1. More Popular" << endl;
+            cout << "2. Less Popular" << endl;
+            cout << "Choose Popularity Level: ";
+            cin >> p;
+            cout << endl;
+            if (p == "1") {
+                for (unsigned int i = 0; i < 10; i++) {
+                    Movie m = maxYear.extractMax();
+                    cout << m.getTitle() << " (" << m.getYear() << ")";
+                    cout << " - Dir. by: " << m.getDirectorName() << endl;
+                    cout << "Popularity Score: " << m.getPopularity() << endl;
+                    cout << endl;
+                }
+            }
+            else if (p == "2") {
+                for (int i = 0; i < 10; i++) {
+                    Movie m = minYear.extractMin();
+                    cout << m.getTitle() << " (" << m.getYear() << ")";
+                    cout << " - Dir. by: " << m.getDirectorName() << endl;
+                    cout << "Popularity Score: " << m.getPopularity() << endl;
+                    cout << endl;
+                }
+            }
+            break;
+        case 6:
+            cout << "\nInsert Director: ";
             string direct;
-            cin >> direct;
-            //map.getDirectorsMovie(map.mMap, direct);
+            getline(cin, direct);
+            getline(cin, direct);
+            cout << endl;
+            dirMovies = map.getDirectorMovie(map.mMap, direct);
+            if (dirMovies.empty())
+                cout << "Name not recognized." << endl;
+            else {
+                for (int i = 0; i < dirMovies.size(); i++) {
+                    Movie m = dirMovies[i];
+                    cout << m.getTitle() << "(" << m.getYear() << ")" << endl;
+                    cout << "Popularity Score: " << m.getPopularity() << endl;
+                    cout << endl;
+                }
+            }
             break;
         }
-
+        cout << endl;
     }
 
     return 0;
